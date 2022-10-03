b3ee9c9d5760dd3a344aa181074e9073dc8986fc
Mikhail Yarmaliuk <mikhail.yarmaliuk@lomray.com>
2022-10-03 22:48:19 +0200

    fix: re-initialize

--- release.config.js
+++ release.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
     '@semantic-release/github',
+    '@semantic-release/npm',
   ]
 }
